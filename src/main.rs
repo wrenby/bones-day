cbbe289db3c6849a8d369bf2df685ea21f7a7616
Wren Baxter <git@wren.dev>
2021-11-04 03:10:14 -0400

    failed axum + maud

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
+/*
+TODO
+
+https://crates.io/crates/egg-mode
+https://github.com/egg-mode-rs/egg-mode/blob/master/examples/stream_filter.rs
+https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api
+*/
+
+use axum::{
+    handler::get,
+    Router,
+};
+use maud::{html, Markup};
+
+async fn hello_world() -> Markup {
+    html! {
+        h1 { "Hello, World!" }
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // build our application with a single route
+    let app = Router::new().route("/", get(hello_world));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
