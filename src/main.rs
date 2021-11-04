5eb01c75ab3e2c41de86175ef93119688f15c594
Wren Baxter <git@wren.dev>
2021-11-04 03:33:12 -0400

    tide + maud hello world

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,21 @@ https://crates.io/crates/egg-mode
 https://github.com/egg-mode-rs/egg-mode/blob/master/examples/stream_filter.rs
 https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api
 */
-
-use axum::{
-    handler::get,
-    Router,
-};
 use maud::{html, Markup};
+use tide::Request;
 
-async fn hello_world() -> Markup {
-    html! {
-        h1 { "Hello, World!" }
-    }
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/hello/:name").get(greet);
+    app.listen("127.0.0.1:3000").await?;
+    Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(hello_world));
-
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+async fn greet(req: Request<()>) -> tide::Result<Markup> {
+    let name: String = req.param("name")?.parse()?;
+    Ok(html! {
+        h1 { "Hello, " (name) "!" }
+        p { "Nice to meet you!" }
+    })
 }
