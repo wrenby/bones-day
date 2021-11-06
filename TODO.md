e1b9bdcaf57e197f224379a2c39e1865f02c295e
Wren Baxter <git@wren.dev>
2021-11-06 00:17:57 -0400

    actix-web + twitter api + git-crypt

---
+++ TODO.md
@@ -0,0 +1,20 @@
+- initialize with most recent *valid* tweet
+- set up stream filter and vibe
+- offset timezones in response
+
+
+ignore retweets and replies
+
+BONES DAY
+---
+#BonesDay
+has bones
+bones day, preceded by anything other than no
+- planning for the future, also handle "not a "
+
+
+NO BONES DAY
+---
+#NoBonesDay
+no bones
+does not have bones
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,28 @@ https://crates.io/crates/egg-mode
 https://github.com/egg-mode-rs/egg-mode/blob/master/examples/stream_filter.rs
 https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api
 */
-use maud::{html, Markup};
-use tide::Request;
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
-    app.at("/hello/:name").get(greet);
-    app.listen("127.0.0.1:3000").await?;
-    Ok(())
+#![feature(once_cell)]
+use actix_web::{ get, web, App, HttpServer, Result as AwResult };
+use egg_mode::Token;
+use std::{ include_str, io };
+use std::lazy::SyncLazy;
+
+static TOKEN: SyncLazy<Token> = SyncLazy::new(|| {
+    Token::Bearer(include_str!("../api/bearer").trim_end().to_string())
+});
+
+#[get("/{username}")]
+async fn info(username: web::Path<String>) -> String {
+    let info = egg_mode::user::show(username.to_string(), &TOKEN).await.unwrap();
+
+    format!("<html><body><h1>{} ({})</h1></body></html>", info.name, info.screen_name)
 }
 
-async fn greet(req: Request<()>) -> tide::Result<Markup> {
-    let name: String = req.param("name")?.parse()?;
-    Ok(html! {
-        h1 { "Hello, " (name) "!" }
-        p { "Nice to meet you!" }
-    })
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().service(info))
+        .bind(("127.0.0.1", 3000))?
+        .run()
+        .await
 }
